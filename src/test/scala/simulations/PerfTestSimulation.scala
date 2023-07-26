bee78f2f112f51ebfee13ea06b6abb11a767779d
Jun Lei <1012762794@qq.com>
2023-07-26 18:17:40 +0800

    update script for load test purpose

--- src/test/scala/simulations/PerfTestSimulation.scala
+++ src/test/scala/simulations/PerfTestSimulation.scala
@@ -4,6 +4,8 @@ import config.BaseHelpers._
 import io.gatling.core.Predef._
 import scenarios.Shopping._
 
+import scala.concurrent.duration.DurationInt
+
 
 class PerfTestSimulation extends Simulation {
 
@@ -14,6 +16,7 @@ class PerfTestSimulation extends Simulation {
   // mvn gatling:test -DUsersCount=100
 
   setUp(
-    scnShopping.inject(atOnceUsers(System.getProperty("UsersCount", "10").toInt))
-  ).protocols(httpProtocol)
+    scnShopping.inject(rampUsers(System.getProperty("UsersCount", "5").toInt).during(10.seconds))
+//    scnShopping.inject(atOnceUsers(System.getProperty("UsersCount", "10").toInt))
+  ).maxDuration(10.minutes).protocols(httpProtocol)
 }
